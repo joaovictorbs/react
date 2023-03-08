47f6eab86296b647177709aa02597833881fafdc
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-03-07 23:22:53 -0300

    Criando o primeiro componente

--- projetosCurso/fundamentos/src/App.js
+++ projetosCurso/fundamentos/src/App.js
@@ -1,10 +1,23 @@
-function App() {
-  return (
-    <div>
-      Hello World!
-      <div>React js é muito legal!</div>
-    </div>
-  );
+import React, { Component } from 'react'  //importa react e componente dentro dele
+
+class Button extends Component {
+  render() {
+    return(
+      <button>Enviar!</button>
+    )
+  }
 }
 
-export default App;
+class App extends Component { //novo componente app que extende do Component
+  render() {    //metodo render dentro de Component que retorna conteudo html
+    return(     
+      <>     
+        <h1>Hello World!</h1>
+        <Button />
+      </>
+      //ou <React.Fragment> ao inves de <>
+    )
+  }
+}
+
+export default App //garante que arquivos fora dele tenham acesso ao componente
