852f10c322651c3e74b51f3780d9b95c5fe87887
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-03-07 23:35:57 -0300

    JSX e expressoes

--- projetosCurso/fundamentos/src/App.js
+++ projetosCurso/fundamentos/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'  //importa react e componente dentro dele
 
+const title = <h2>Olá mundo!</h2> //tag nao e html simples e sim JSX
+
+function formatName(name, lastname) {
+  return name + lastname;
+}
+
 class Button extends Component {
   render() {
     return(
-      <button>Enviar!</button>
+      <button>Enviar! {  2 + 2 }</button>  //JSX | nao e html puro
     )
   }
 }
 
 class App extends Component { //novo componente app que extende do Component
-  render() {    //metodo render dentro de Component que retorna conteudo html
+  render() {                  //metodo render dentro de Component que retorna conteudo html
     return(     
       <>     
-        <h1>Hello World!</h1>
+        { title }                                
+        <h1>Hello World! { formatName('Joao ', 'Barbosa') }</h1>
         <Button />
       </>
       //ou <React.Fragment> ao inves de <>
