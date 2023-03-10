f66fce43c8ce3b444265bfdd84065bde72037638
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-03-10 20:36:26 -0300

    aplicando CSS external

--- projetosCurso/fundamentos/src/App.js
+++ projetosCurso/fundamentos/src/App.js
@@ -1,16 +1,5 @@
 import React, { Component } from 'react'
-
-const styles = {        //cria objeto com valores CSS
-  container: {
-    backgroundColor: '#333',
-    padding: '20px'
-  },
-  title: {
-    fontSize: '60px',
-    fontFamily: 'sans-serif',
-    color: '#fff'
-  }
-}
+import './styles.css'
 
 class App extends Component { 
   constructor(props) {
@@ -19,11 +8,8 @@ class App extends Component {
 
   render() {                  
     return(     
-      <div style={styles.container}>  {/*aplica na div os estilos do container*/}
+      <div className='container'>  {/*usamos className ao inves de class pois class ja e uma palavra reservada do JS */}
         <h1>Olá Mundo!</h1>
-        <h1 style={styles.title}>Olá Mundo!</h1>
-        <h3 style={{ backgroundColor: '#f33', color: '#fff', fontSize: '40px' }}>Olá Mundo!</h3>    {/*aplicando css direto no elemento*/} {/*em css inline, precisa ser camelCase */}
-  
       </div>
     )
   }
