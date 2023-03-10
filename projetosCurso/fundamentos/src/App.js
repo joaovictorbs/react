9e01601a4f14d9af4f93d9dbf42a55731db25439
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-03-10 19:15:59 -0300

    Renderizacao de listas

--- projetosCurso/fundamentos/src/App.js
+++ projetosCurso/fundamentos/src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
-import Button from './Button' //importa arquivo Button
+
+const names = ['Luis', 'Natalia', 'João', 'Maria', 'Camila', 'Felipe']  //lista
+
+/*
+const names = [   //array de objetos
+  {
+    name: 'Luis',
+    age: 19,
+  },
+  {
+    name: 'Natalia',
+    age: 27,
+  },
+  {
+    name: 'Lucas',
+    age: 22,
+  }  
+] */ //lista
 
 class App extends Component { 
+  constructor(props) {
+    super()
+  }
+
   render() {                  
     return(     
-      <>     
-        <Button label="Entrar"/><br />
-      </>
+      <ul>                         {/*lista html*/}
+      {
+        names.map((name, index) => (  //para cada indice retorna elemento
+          <li key={index}>{ name }</li>  //para cada nome cria um li  | o key ajuda o react a saber como o elemento esta, se foi removido, adiconado, etc
+
+        ))   
+      }     
+      </ul>
     )
   }
 }
