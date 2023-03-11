d0b6a1c3e220e227649e17eec352d6b8dabbb209
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-03-11 19:08:34 -0300

    useState

--- projetosCurso/fundamentos/src/App.js
+++ projetosCurso/fundamentos/src/App.js
@@ -1,35 +1,11 @@
-//Componente de Funcao
+import Estado from "./Estado";
+
  function App() {
    return (
      <div>
-       Hello World: Function <br/><br/>
-       <Button title="Entrar" numero={10}/>
+      <Estado />
      </div>
    );
  }
-
-//function Button(props) {                  //com componentes de funcao definimos as props aqui ao inves de montar um construtor igual na classe
-//function Button({ title, numero }) {      //podemos omitir a nomenclatura props. e apenas passar a propriedade
-const Button = ({ title, numero }) => {        //podemos criar componentes como se fossem constantes, basta adicionar o const e a => (arrowFunction)
-  return (
-    <button>{ title } - { numero }</button>
-  )
-}
-
-
-//Componente de Classe
-/*
-import React, { Component } from "react";
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World: Class
-      </div>
-    )
-  }
-}
-*/
 
 export default App;
